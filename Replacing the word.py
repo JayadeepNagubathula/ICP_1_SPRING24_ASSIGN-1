c75359c9a6edc788119e0ccc2469e2d8e47d6009
JayadeepNagubathula <156541783+JayadeepNagubathula@users.noreply.github.com>
2024-01-15 23:15:34 -0600

    Update Replacing the word.py

--- Replacing the word.py
+++ Replacing the word.py
@@ -1,3 +1,5 @@
 GivenString = str(input("enter the string:")) #taking the input from the user
-newstring = GivenString.replace("python", "pythons") #replacing the words with replace keyword
+newstring = GivenString.replace("python", "pythons")#replacing the words with replace keyword
+newstring = txt.replace("Python", "Pythons")
+newstring = txt.replace("PYTHON", "PYTHONS")
 print("Newstring after replacing is:" + newstring) #printing the result string
